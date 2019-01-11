e8d1f71c039000d2b26ae063dd58fe477b156a7b
Dani Martínez <danimartinezsaa@gmail.com>
2019-01-11 10:54:08 +0100

    Ejemplo Abrir otras Activitys con Anko y Master Details

---
+++ app/src/main/java/com/dani/aplicacionmasterdetails/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.dani.aplicacionmasterdetails
+
+import android.content.res.Configuration
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Preguntamos la orientación en la que estamos
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            toast("Estás en landscape")
+
+        } else{
+            toast("Estás en vertical")
+        }
+
+        activity2.setOnClickListener {
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                toast("¡HOLA!")
+            } else{
+                //Lanzar otra Activity con Anko
+                startActivity<SegundaActivity>()
+            }
+        }
+
+        marca.setOnClickListener {
+            //Abrir url con el navegador
+            browse("https://www.marca.com")
+        }
+    }
+}
